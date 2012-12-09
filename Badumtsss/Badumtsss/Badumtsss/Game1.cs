7809c9e88211cae74b81175968a1754430aa339b
Ciprian Tomoiaga <ciprian.tomoiaga@gmail.com>
2012-12-09 07:51:04 +0000

    detects basic beam and mirror interraction

--- Badumtsss/Badumtsss/Badumtsss/Game1.cs
+++ Badumtsss/Badumtsss/Badumtsss/Game1.cs
@@ -8,6 +8,7 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using Badumtsss.Objects;
 
 namespace Badumtsss
 {
@@ -19,7 +20,7 @@ namespace Badumtsss
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Texture2D lightTexture;
-        Vector2[] obstacles = new Vector2[3];
+        Obstacle[] obstacles = new Obstacle[3];
 
         public Game1()
         {
@@ -54,9 +55,9 @@ namespace Badumtsss
             lightTexture = Content.Load<Texture2D>("light_tex");
 
             // Initialize obstacles
-            obstacles[0] = new Vector2(50, 50);
-            obstacles[1] = new Vector2(350, 50);
-            obstacles[2] = new Vector2(350, 500);
+            obstacles[0] = new Obstacle(Obstacle.ObjectType.MIRROR, new Vector2(150, 10), 5, 15, 0f);
+            obstacles[1] = new Obstacle(Obstacle.ObjectType.MIRROR, new Vector2(300, 10), 5, 15, 0f);
+            obstacles[2] = new Obstacle(Obstacle.ObjectType.MIRROR, new Vector2(100), 5, 15, 0f);
             
 
             // TODO: use this.Content to load your game content here
@@ -96,6 +97,26 @@ namespace Badumtsss
             spriteBatch.Draw(lightTexture, new Rectangle((int)source.X, (int)source.Y, (int)distance, 2), null, Color.White, angle, Vector2.Zero, SpriteEffects.None, 0);
             spriteBatch.End();
         }
+
+        private Obstacle FindObject(Obstacle[] obstacles, Vector2 start, short direction, float angle)
+        {
+            //update 1300 with MaxWidth
+            RotatedRectangle r = new RotatedRectangle(new Rectangle((int)start.X, (int)start.Y, 1300, 2), angle);
+            float dmin = 1300;
+            float distance;
+            Obstacle colidedObstacle = null;
+            foreach (var obstacle in obstacles)
+            {
+                distance = Vector2.Distance(start, obstacle.A);
+                if (r.Intersects(obstacle) && distance < dmin && distance > 0)
+                {
+                    dmin = Vector2.Distance(start, obstacle.A);
+                    colidedObstacle = obstacle;
+                }
+            }
+            return colidedObstacle;
+        }
+
         /// <summary>
         /// This is called when the game should draw itself.
         /// </summary>
@@ -106,10 +127,32 @@ namespace Badumtsss
 
             // TODO: Add your drawing code here
             //  DrawLine(obstacles[0], obstacles[1]);
-            for (int index = 0; index <= 1; index++)
-                DrawLine(obstacles[index], obstacles[index + 1]);
+
+            Vector2 start = new Vector2(10);
+            Obstacle nextObstacle = FindObject(obstacles, start, 1, 0);
+            Obstacle currentObstactle = nextObstacle;
+            DrawLine(start, nextObstacle.A);
+            MouseState currentState = Mouse.GetState();
+            MouseState previousState = new MouseState(0, 0, 0, ButtonState.Released, ButtonState.Released, ButtonState.Released, ButtonState.Released, ButtonState.Released);
+            this.IsMouseVisible = true;
+            if (currentState.LeftButton == ButtonState.Pressed || previousState.LeftButton != ButtonState.Pressed)
+            {
+                while ((nextObstacle = FindObject(obstacles, nextObstacle.A, 1, nextObstacle.Rotation)) != null)
+                {
+                    DrawLine(currentObstactle.A, nextObstacle.A);
+                    currentObstactle = nextObstacle;
+                }
+                previousState = currentState;
+            }
+
+            /*
+            DrawLine(new Vector2(10, 10), obstacles[0].A);
+            for (int index = 0; index < 2; ++index)
+                DrawLine(obstacles[index].A, obstacles[index + 1].A);
+             */
+            
             Rectangle a = new Rectangle();
-            a.
+            Obstacle o = new Obstacle(Obstacle.ObjectType.MIRROR, new Vector2(10), 30, 30, 0f);
             base.Draw(gameTime);
         }
     }
---
+++ Badumtsss/Badumtsss/Badumtsss/Objects/Obstacle.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Badumtsss.Objects;
+
+namespace Badumtsss
+{
+    class Obstacle : RotatedRectangle
+    {
+        private Vector2 a, b, c, d;
+        private ObjectType type;
+        private Texture2D texture;
+        
+        public enum ObjectType
+        {
+            MIRROR,
+            WALL
+        };
+
+        #region Properties
+
+        public Enum Type
+        {
+            get { return type; }
+            set { type = (ObjectType)value; }
+        }
+
+        public Vector2 A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+
+        public Vector2 B
+        {
+            get { return b; }
+            set { b = value; }
+        }
+
+        public Vector2 C
+        {
+            get { return c; }
+            set { c = value; }
+        }
+
+        public Vector2 D
+        {
+            get { return d; }
+            set { d = value; }
+        }
+        #endregion
+
+        /* Old constructor
+        /// <summary>
+        /// Creates a new obstacle, as a rectangle within the given points
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="d"></param>
+        public Obstacle(ObjectType type, Vector2 a, Vector2 b, Vector2 c, Vector2 d)
+            : base(new Rectangle(a.X, a.Y, Vector2.Distance(b, a), Vector2.Distance(c, b)), Vector2.Distance(
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+            this.type = type;
+            setTexture(type);
+        }*/
+
+        /// <summary>
+        /// Creates a new Obstacle as a rectangle
+        /// </summary>
+        /// <param name="location">Location of the upper left corner</param>
+        /// <param name="width">width of the rectangle</param>
+        /// <param name="height">length of the rectangle</param>
+        /// <param name="initialRotation">The initial rotation of the object</param>
+        public Obstacle(ObjectType type, Vector2 location, float width, float height, float initialRotation)
+            : base(new Rectangle((int)location.X, (int)location.Y, (int)width, (int)height), initialRotation)
+        {
+            a = UpperLeftCorner();
+            b = UpperRightCorner();
+            c = LowerRightCorner();
+            d = LowerLeftCorner();
+
+            //setTexture(type);
+        }
+
+        #region Methods
+
+        private void setTexture(ObjectType type)
+        {
+            Game1 game = new Game1();
+            if (type == ObjectType.MIRROR)
+                texture = game.Content.Load<Texture2D>("mirror_tex");
+        }
+
+        private void Rotate(float rotation)
+        {
+            this.rotation += rotation;
+            a = UpperLeftCorner();
+            b = UpperRightCorner();
+            c = LowerRightCorner();
+            d = LowerLeftCorner();
+        }
+
+        #endregion
+
+    }
+}
